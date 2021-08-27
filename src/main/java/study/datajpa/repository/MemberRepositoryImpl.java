896f6f18c870929b6d05147d59d46aad2ccbc1e8
seno-b <bangshinho@gmail.com>
2021-08-27 23:37:42 +0900

    add: querydsl 다중 where 조건 처리

--- src/main/java/study/datajpa/repository/MemberRepositoryImpl.java
+++ src/main/java/study/datajpa/repository/MemberRepositoryImpl.java
@@ -1,10 +1,13 @@
 package study.datajpa.repository;
 
 import lombok.RequiredArgsConstructor;
+import org.hibernate.query.criteria.internal.predicate.BooleanExpressionPredicate;
 import study.datajpa.entity.Member;
 
 import javax.persistence.EntityManager;
 import java.util.List;
+import java.util.Objects;
+import java.util.function.Predicate;
 
 @RequiredArgsConstructor
 public class MemberRepositoryImpl implements MemberRepositoryCustom {
@@ -15,5 +18,13 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom {
     public List<Member> findMemberCustom() {
         return em.createQuery("select m from Member m", Member.class)
                 .getResultList();
+    }
+
+    private Predicate usernameEq(String username) {
+        if (Objects.isNull(username)) {
+            return null;
+        }
+
+        return null;
     }
 }
